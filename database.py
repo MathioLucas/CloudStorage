606d27205edf5c54278ea1f47e162ffe07405cff
MathioLucas <162763121+MathioLucas@users.noreply.github.com>
2025-03-03 19:24:42 -0500

    Update database.py

--- database.py
+++ database.py
@@ -22,3 +22,37 @@ CREATE TABLE files(
     
     
 );
+
+CREATE TABLE folders(
+    id SERIAL PRIMARY KEY,
+    folder_name varchar(20) NOT NULL,
+    date_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    date_modified TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    owner_id int REFRENCES users(id) ON DELETE CASCADE,
+
+);
+
+CREATE TABLE file_versions(
+    id SERIAL PRIMARY KEY,
+    file_id int REFRENCES files(id) ON DELETE CASCADE,
+    version_number int NOT NULL,
+    file_name varchar(20) NOT NULL,
+    file_type varchar(20) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    
+    
+    
+    
+    
+);
+CREATE TABLE shared_files(
+    id SERIAL PRIMARY KEY,
+    file_id int REFRENCES files(id) ON DELETE CASCADE,
+    folder_id int REFRENCES folders(id) ON DELETE SET NULL,
+    share_name varchar(20) NOT NULL,
+    share_link varchar(20) NOT NULL,
+    permissions VARCHAR(50) DEFAULT 'read' 
+    
+    
+    
+);
